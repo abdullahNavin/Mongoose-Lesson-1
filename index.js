7290718d250bffc0f36c9e4ec7441edbea6cbd01
abdullahNavin <navinfayed26@gmail.com>
2025-04-28 19:48:47 +0600

    improve security

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -11,10 +12,9 @@ app.use(cors());
 app.use(express.json());
 
 //mongoose connection
-mongoose.connect(`mongodb+srv://MainAdminForAll:kGhPVQA0bQVZxDJ4@simple-crud.ce5cqwx.mongodb.net/?retryWrites=true&w=majority&appName=Simple-crud`)
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@simple-crud.ce5cqwx.mongodb.net/?retryWrites=true&w=majority&appName=Simple-crud`)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
-
 
 
 //default error handler
